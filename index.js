9401b619e27a8e0246e746923191d1029b5bae72
Brian Ndickers <bryondickers@gmail.com>
2023-06-17 16:55:49 +0300

    rating response

---
+++ index.js
@@ -0,0 +1,33 @@
+var rateNumber = undefined;
+
+function getContent(element) {
+    rateNumber = element.textContent;
+    
+    element.classList.add("bg-orange","text-white")
+    console.log(element)
+    console.log(element.currentTarget);
+  
+    
+};
+
+
+document.getElementById("submit-btn").addEventListener("click",selectRating)
+
+
+function selectRating(){
+    let outputRate = document.getElementById("output-value") 
+    if(rateNumber){
+        outputRate.textContent = rateNumber;
+    
+        let firstCard = document.getElementById("initial-article")
+        firstCard.classList.add("hidden");
+        let feedBack = document.getElementById("feedback-note");
+        feedBack.classList.remove("hidden");
+    }
+    else {
+        alert("please select rate number")
+    }
+}
+
+
+
